9f855ab16b7151c51ad0d478bddd7a76e307e6c3
Ethan Keirs <60351308+EthanKeirs@users.noreply.github.com>
2021-05-30 04:46:54 +1000

    Add files via upload

---
+++ Master.py
@@ -0,0 +1,109 @@
+import gym
+from src.driver import Driver
+from src.agents.random import Random
+from src.agents.qlearner import Qlearner
+from src.agents.tdlearner import TDlearner
+
+
+def taxi_random():
+    agent = Random()
+    driver = Driver({
+        'epochs': 1000,
+        'env': gym.make('Taxi-v3'),
+        'agent': agent,
+    })
+    driver.run_taxi_random()
+
+def taxi_qlearner():
+    agent = Qlearner({
+        'alpha': 0.1,
+        'gamma': 0.6,
+        'epsilon': 0.1,
+    })
+    driver = Driver({
+        'epochs': 10000,
+        'env': gym.make('Taxi-v3'),
+        'agent': agent,
+    })
+    driver.run_taxi_qlearner()
+
+def cartpole_random():
+    agent = Random()
+    driver = Driver({
+        'epochs': 1000,
+        'env': gym.make('CartPole-v1'),
+        'agent': agent,
+    })
+    driver.run_cartpole_random()
+
+def cartpole_qlearner():
+    agent = Qlearner({
+        'alpha': 0.2,
+        'gamma': 0.5,
+        'epsilon': 0.1,
+    })
+    driver = Driver({
+        'epochs': 50000,
+        'env': gym.make('CartPole-v1'),
+        'agent': agent,
+    })
+    driver.run_cartpole_qlearner()
+
+def cartpole_tdlearner():
+    agent = TDlearner({
+        'alpha': 0.2,
+        'gamma': 0.5,
+        'epsilon': 0.1,
+    })
+    driver = Driver({
+        'epochs': 50000,
+        'env': gym.make('CartPole-v1'),
+        'agent': agent,
+    })
+    driver.run_cartpole_tdlearner()
+
+def frozen_lake_random():
+    agent = Random()
+    driver = Driver({
+        'epochs': 1000,
+        'env': gym.make('FrozenLake-v0'),
+        'agent': agent,
+    })
+    driver.run_frozen_lake_random()
+
+def frozen_lake_qlearner():
+    agent = Qlearner({
+        'alpha': 0.1,
+        'gamma': 0.6,
+        'epsilon': 0.3,
+    })
+    driver = Driver({
+        'epochs': 10000,
+        'env': gym.make('FrozenLake-v0'),
+        'agent': agent,
+    })
+    driver.run_frozen_lake_qlearner()
+
+def frozen_lake_tdlearner():
+    agent = TDlearner({
+        'alpha': 0.1,
+        'gamma': 0.6,
+        'epsilon': 0.3,
+    })
+    driver = Driver({
+        'epochs': 10000,
+        'env': gym.make('FrozenLake-v0'),
+        'agent': agent,
+    })
+    driver.run_frozen_lake_tdlearner()
+
+if __name__ == '__main__':
+    taxi_random()
+    #taxi_qlearner()
+    #cartpole_random()
+    #cartpole_qlearner()
+    #cartpole_tdlearner()
+    #frozen_lake_random()
+    #frozen_lake_qlearner()
+    #frozen_lake_tdlearner()
+
